19758574a89decbfa213f869b322bfd03050e465
Thierry Gribeauval <t.gribeauval11@gmail.com>
2021-07-16 19:08:38 -0400

    error

--- curl.js
+++ curl.js
@@ -1,7 +1,7 @@
 $.ajax({
         url: 'https://www.strava.com/api/v3/athlete',
         beforeSend: function(xhr) {
-             xhr.setRequestHeader("Authorization", "Bearer 6426588e45355ea5057c6950610c0fd476e95b3f")
+             xhr.setRequestHeader("Authorization", "Bearer API KEY")
         }, success: function(data){
             console.log(data);
             //process the JSON data etc
